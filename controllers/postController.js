5308d8540df56c10a7c61f2402788020b1bfc173
diegotosato <tosato1998@gmail.com>
2025-11-26 12:35:52 +0100

    create new folder for controller and file for posts controller, declare all CRUD functions

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+//importo il menu
+const menu = require('../data/menu')
+
+//preparo tutte le funzioni
+
+//index
+function index(req, res) {
+
+}
+
+//show
+function show(req, res) {
+
+}
+
+//store
+function store(req, res) {
+
+}
+
+//update
+function put(req, res) {
+
+}
+
+//modify
+function patch(req, res) {
+
+}
+
+//delete
+function destroy(req, res) {
+
+}
