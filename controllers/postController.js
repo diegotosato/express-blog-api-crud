32c8a7b1e9729d446bc77e4b9ca0899a3ad40daa
diegotosato <tosato1998@gmail.com>
2025-11-26 12:41:58 +0100

    copy all functions logic from router into controller, export all functions, in router imiiport the controller and connect each route to its controller function

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,32 +3,34 @@ const menu = require('../data/menu')
 
 //preparo tutte le funzioni
 
-//index
+//index (.get)
 function index(req, res) {
-
+    res.json(menu)
 }
 
-//show
+//show (.get)
 function show(req, res) {
-
+    res.json(menu.find(post => post.id === Number(req.params.id)))
 }
 
-//store
+//store (.post)
 function store(req, res) {
-
+    res.send('Aggiungi un nuovo post')
 }
 
-//update
-function put(req, res) {
-
+//update (.put)
+function update(req, res) {
+    res.send(`Modifica INTERAMENTE il post con id: ${req.params.id}`)
 }
 
-//modify
-function patch(req, res) {
-
+//modify (.patch)
+function modify(req, res) {
+    res.send(`Modifica PARZIALMENTE il post con id: ${req.params.id}`)
 }
 
-//delete
+//destroy (.delete)
 function destroy(req, res) {
-
+    res.send(`Cancellazione del post con id: ${req.params.id}`)
 }
+
+module.exports = { index, show, store, update, modify, destroy }
--- routers/posts.js
+++ routers/posts.js
@@ -1,38 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const menu = require('../data/menu')
+
+//importo il controller
+const postController = require('../controllers/postController')
 
 //index
-router.get('/', (req, res) => {
-    // res.send('Lista dei i post')
-    res.json(menu)
-})
+router.get('/', postController.index)
 
 //show
-router.get('/:id', (req, res) => {
-    // res.send(`Mostrami il post con id: ${req.params.id}`)
-    res.json(menu.find(post => post.id === Number(req.params.id)))
-})
+router.get('/:id', postController.show)
 
 //store
-router.post('/', (req, res) => {
-    res.send('Aggiungi un nuovo post')
-})
+router.post('/', postController.store)
 
 //update
-router.put('/:id', (req, res) => {
-    res.send(`Modifica INTERAMENTE il post con id: ${req.params.id}`)
-})
+router.put('/:id', postController.update)
 
 //modify
-router.patch('/:id', (req, res) => {
-    res.send(`Modifica PARZIALMENTE il post con id: ${req.params.id}`)
-})
+router.patch('/:id', postController.modify)
 
 //destroy
-router.delete('/:id', (req, res) => {
-    res.send(`Cancellazione del post con id: ${req.params.id}`)
-})
+router.delete('/:id', postController.destroy)
 
 
 module.exports = router;
