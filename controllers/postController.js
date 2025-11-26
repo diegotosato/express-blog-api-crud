1cb3cdd78d850a9f935e23cef642eacab45c8777
diegotosato <tosato1998@gmail.com>
2025-11-26 12:51:07 +0100

    update show logic, more readable, implement destroy logic finding the object to remove by id, and remove from the array with splice method, return the status updated to 204

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,7 +10,14 @@ function index(req, res) {
 
 //show (.get)
 function show(req, res) {
-    res.json(menu.find(post => post.id === Number(req.params.id)))
+    //converto l'ID in numero
+    const id = Number(req.params.id)
+
+    //filtro il menu ed estraggo il sinoglo post il cui id è uguale ad id
+    const findPost = menu.find(post => post.id === id)
+
+    //restituisco in formato json il post trovato
+    res.json(findPost)
 }
 
 //store (.post)
@@ -30,7 +37,22 @@ function modify(req, res) {
 
 //destroy (.delete)
 function destroy(req, res) {
-    res.send(`Cancellazione del post con id: ${req.params.id}`)
+    //converto l'ID in numero
+    const id = Number(req.params.id)
+
+    //filtro il menu ed estraggo il sinoglo post il cui id è uguale ad id
+    const deletePost = menu.find(post => post.id === id)
+
+    //metodo splice per rimuovere il post desiderato
+    //recupero la posizione nell'array del post da eliminare, dichiaro dopo la virgola quanti elementi rimuovere
+    menu.splice(menu.indexOf(deletePost), 1);
+
+    //loggo in console l'array aggiornato
+    console.log(menu);
+
+    //rispondo on un errore 204 per indicare che la cancellazione sia avvenuta con successo
+    res.sendStatus(204)
+
 }
 
 module.exports = { index, show, store, update, modify, destroy }
