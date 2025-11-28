f0f18ab9a1e59004a2fbc4e075abdd892599d4ed
diegotosato <tosato1998@gmail.com>
2025-11-28 11:22:35 +0100

    create new folder for middlewares, declare a middleware to handle an error, export it and import in server, put at global level after all routes, test in postman

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+//importo la middleware per gestire gli errori
+const handleError = require('./middlewares/handleError')
+
 const postsRouter = require('./routers/posts')
 
 //imposto gli asset statici
@@ -12,14 +15,18 @@ app.use(express.json())
 
 
 
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
 
 app.get('/', (req, res) => {
+    diego.daje()
     res.send('Home Page')
 })
 
 //Resource: Posts
 app.use('/api/posts', postsRouter)
+
+
+//la dichiaro a livello globale alla fine, dopo tutte le rotte
+app.use(handleError)
